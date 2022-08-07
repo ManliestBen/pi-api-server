c01ed6c184aa14690787dd72abc5ee8eb133af0f
Ben Manley <benjamintmanley@gmail.com>
2022-08-06 21:09:24 -0500

    add OLED message display to server

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from flask import Flask, request, Response
 from flask_cors import CORS
 import RPi.GPIO as GPIO
-import time
+import time, math
+from pathlib import Path
+from luma.core.virtual import viewport
+from luma.core.render import canvas
+from PIL import ImageFont
+from luma.oled.device import ssd1306
+from luma.core.interface.serial import i2c
 
 
 app = Flask(__name__)
 CORS(app)
 
+serial = i2c(port=1, address=0x3C)
+
 color_r = 0
 color_g = 0
 color_b = 0
+
+def make_font(name, size):
+    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
+    return ImageFont.truetype(font_path, size)
+
+font = make_font("ProggyTiny.ttf", 30)
 
 @app.route('/rgb', methods=['POST'])
 def process_json():
@@ -22,9 +36,38 @@ def process_json():
     else:
         return 'Content-Type not supported!'
 
+@app.route('/message', methods=['POST'])
+def process_json():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        message = request.json["message"]
+        return display_message(message)
+    else:
+        return 'Content-Type not supported!'
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def display_message(message):
+    device = ssd1306(serial, width=128, height=32)
+    virtual = viewport(device, width=1360, height=768)
+
+    with canvas(virtual) as draw:
+        draw.text((0, 12), "New Message!", font=font, fill="white")
+
+    time.sleep(3)
+
+    with canvas(virtual) as draw:
+        draw.text((0,12), message, font=font, fill="white")
+
+    time.sleep(2)
+
+    for x in range(int(len(message)*11)):
+        virtual.set_position((x, 0))
+        time.sleep(0.001)
+    
+    return Response("Successful", status=201, mimetype='application/json')
 
 def set_rgb(r, g, b):
     pins = (11,13,15) 
